93ad20f917692ae0db841fc0ea80294cd1aed200
Iain Nash <iain@codejoust.com>
2015-03-01 05:56:43 +0100

    pushhh

--- android/SingAndLearn/app/src/main/java/org/khacks/singandlearn/datastore/Song.java
+++ android/SingAndLearn/app/src/main/java/org/khacks/singandlearn/datastore/Song.java
@@ -3,6 +3,7 @@ package org.khacks.singandlearn.datastore;
 import android.database.Cursor;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
